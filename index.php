296d9edba1f173ad3dded638e2d30845d2115200
ndiayemtr <ndiayemtr@gmail.com>
2021-07-27 09:56:21 +0000

    Code HTML & CSS Start

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=10">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Realtime Chat App | Ndiayemtr</title>
+</head>
+<body>
+	<div class="wrapper">
+		<section class="form signup">
+			<header>Realtime Chat App</header>
+			<form action="#">
+				<div class="error-txt">This is an error message!</div>
+				<div class="name-details">
+					<div class="field">
+						<label>First Name</label>
+						<input type="text" placeholder="First Name">
+					</div>
+					<div class="field">
+						<label>Lirst Name</label>
+						<input type="text" placeholder="Lirst Name">
+					</div>
+					<div class="field">
+						<label>Email Address</label>
+						<input type="email" placeholder="enter your email">
+					</div>
+					<div class="field">
+						<label>Passwod</label>
+						<input type="passwod" placeholder="Enter new password">
+					</div>
+					<div class="field">
+						<label>Select Image</label>
+						<input type="file" >
+					</div>
+					<div class="field">
+						<input type="submit" value="Continue to Chat">
+					</div>
+				</div>
+			</form>
+			<div class="link">Already signed up? <a href="">Login now</a></div>
+		</section>		
+	</div>
+
+</body>
+</html>
