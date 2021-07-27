482fd985a0db12d1d63341f7c52c997ff0eefa6f
ndiayemtr <ndiayemtr@gmail.com>
2021-07-27 16:13:11 +0000

    Creating Chat Area Page (HTML) and Styling Chat Area Page (CSS)

---
+++ chat.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=10">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Realtime Chat App | Ndiayemtr</title>
+</head>
+<body>
+	<div class="wrapper">
+		<section class="chat-area">
+			<header>
+				<a href="" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+				<img src="images/profil4.png" alt="">
+				<div class="details">
+					<span>ndiayemtr</span>
+					<p>Active now</p>
+				</div>
+			</header>
+			<div class="chat-box ">
+				<div class="chat outgoing">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+					</div>
+				</div>
+					<div class="chat incoming">
+					<img src="images/profil4.png">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+				<div class="chat outgoing">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+					</div>
+				</div>
+					<div class="chat incoming">
+					<img src="images/profil4.png">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+				<div class="chat outgoing">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+					</div>
+				</div>
+					<div class="chat incoming">
+					<img src="images/profil4.png">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+				<div class="chat outgoing">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+					</div>
+				</div>
+					<div class="chat incoming">
+					<img src="images/profil4.png">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+				<div class="chat outgoing">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+					</div>
+				</div>
+					<div class="chat incoming">
+					<img src="images/profil4.png">
+					<div class="details">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+			</div>
+			<form class="typing-area">
+				<input type="text" placeholder="Enter your message..." name="">
+				<button><i class="fab fa-telegram-plane"></i></button>
+			</form>
+		</section>		
+	</div>
+
+</body>
+</html>
+
