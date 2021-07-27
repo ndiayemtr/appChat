c9df8cfe559ddc55d92786d7004098728d81967c
ndiayemtr <ndiayemtr@gmail.com>
2021-07-27 13:50:14 +0000

    Creating Users List Page (HTML) and Styling Users List Page (CSS)

---
+++ user.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=10">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Realtime Chat App | Ndiayemtr</title>
+</head>
+<body>
+	<div class="wrapper">
+		<section class="users">
+			<header>
+				<div class="content">
+					<img src="images/profil4.png" alt="">
+					<div class="details">
+						<span>ndiayemtr</span>
+						<p>Active now</p>
+					</div>
+				</div>
+				<a href="" class="logout">Logout</a>
+			</header>
+			<div class="search">
+				<span class="text">Select an user to start chat</span>
+				<input type="text" placeholder="Enter name to search..." name="">
+				<button><i class="fas fa-search"></i></button>
+			</div>
+			<div class="users-list">
+				<a href="">
+					<div class="content">
+						<img src="images/profil4.png">
+						<div class="details">
+							<span>ndiayemtr</span>
+							<p>This is test message</p>
+						</div>
+					</div>
+					<div class="status-dot"><i class="fas fa-circle"></i></div>
+				</a>
+
+				<a href="">
+					<div class="content">
+						<img src="images/profil4.png">
+						<div class="details">
+							<span>ndiayemtr</span>
+							<p>This is test message</p>
+						</div>
+					</div>
+					<div class="status-dot"><i class="fas fa-circle"></i></div>
+				</a>
+				<a href="">
+					<div class="content">
+						<img src="images/profil4.png">
+						<div class="details">
+							<span>ndiayemtr</span>
+							<p>This is test message</p>
+						</div>
+					</div>
+					<div class="status-dot"><i class="fas fa-circle"></i></div>
+				</a>
+				<a href="">
+					<div class="content">
+						<img src="images/profil4.png">
+						<div class="details">
+							<span>ndiayemtr</span>
+							<p>This is test message</p>
+						</div>
+					</div>
+					<div class="status-dot"><i class="fas fa-circle"></i></div>
+				</a>
+				<a href="">
+					<div class="content">
+						<img src="images/profil4.png">
+						<div class="details">
+							<span>ndiayemtr</span>
+							<p>This is test message</p>
+						</div>
+					</div>
+					<div class="status-dot"><i class="fas fa-circle"></i></div>
+				</a>
+				<a href="">
+					<div class="content">
+						<img src="images/profil4.png">
+						<div class="details">
+							<span>ndiayemtr</span>
+							<p>This is test message</p>
+						</div>
+					</div>
+					<div class="status-dot"><i class="fas fa-circle"></i></div>
+				</a>
+				<a href="">
+					<div class="content">
+						<img src="images/profil4.png">
+						<div class="details">
+							<span>ndiayemtr</span>
+							<p>This is test message</p>
+						</div>
+					</div>
+					<div class="status-dot"><i class="fas fa-circle"></i></div>
+				</a>
+			</div>
+		</section>		
+	</div>
+
+</body>
+</html>
