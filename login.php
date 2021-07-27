4c49a3cbd1117c682642b5f646f22ab8dd1ba8aa
ndiayemtr <ndiayemtr@gmail.com>
2021-07-27 11:25:37 +0000

    Creating Login Form (HTML)

--- login.php
+++ login.php
@@ -4,44 +4,32 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=10">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Realtime Chat App | Ndiayemtr</title>
 </head>
 <body>
 	<div class="wrapper">
-		<section class="form signup">
+		<section class="form login">
 			<header>Realtime Chat App</header>
 			<form action="#">
 				<div class="error-txt">This is an error message!</div>
-				<div class="name-details">
-					<div class="field input">
-						<label>First Name</label>
-						<input type="text" placeholder="First Name">
-					</div>
-					<div class="field input">
-						<label>Lirst Name</label>
-						<input type="text" placeholder="Lirst Name">
-					</div>
-					</div>
+				
 					<div class="field input">
 						<label>Email Address</label>
 						<input type="email" placeholder="enter your email">
 					</div>
 					<div class="field input">
 						<label>Passwod</label>
-						<input type="passwod" placeholder="Enter new password">
+						<input type="passwod" placeholder="Enter your password">
 						<i class="fas fa-eye"></i>
 					</div>
-					<div class="field image">
-						<label>Select Image</label>
-						<input type="file" >
-					</div>
+					
 					<div class="field button">
 						<input type="submit" value="Continue to Chat">
 					</div>			
 			</form>
-			<div class="link">Already signed up? <a href="">Login now</a></div>
+			<div class="link">Not yet signed up? <a href="">Signup now</a></div>
 		</section>		
 	</div>
 
